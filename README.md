edbef56086ca7ba1700b95d231eeb7565a87e6ca
Jester <ABooth9422@gmail.com>
2019-07-07 12:07:19 -0400

    readme updated

--- README.md
+++ README.md
@@ -1,1 +1,37 @@
-# InventoryMgr
+<h1 align="center">Welcome to InventoryMgr 👋</h1>
+<p>
+  <img src="https://img.shields.io/badge/version-1.0.0-blue.svg?cacheSeconds=2592000" />
+  <a href="https://github.com/ABooth9422/InventoryMgr/blob/master/README.md">
+    <img alt="Documentation" src="https://img.shields.io/badge/documentation-yes-brightgreen.svg" target="_blank" />
+  </a>
+  <a href="https://github.com/ABooth9422/InventoryMgr/graphs/commit-activity">
+    <img alt="Maintenance" src="https://img.shields.io/badge/Maintained%3F-yes-green.svg" target="_blank" />
+  </a>
+</p>
+
+> Node & MYSQL project to show how node can join mysql with responses
+
+### 🏠 [Homepage](https://abooth9422.github.io/InventoryMgr/)
+
+## Usage
+
+```sh
+node bamazonCustomer.js
+```
+
+## Author
+
+👤 **Aaron Booth**
+
+* Github: [@ABooth9422](https://github.com/ABooth9422)
+
+## 🤝 Contributing
+
+Contributions, issues and feature requests are welcome!<br />Feel free to check [issues page](https://github.com/ABooth9422/InventoryMgr/issues).
+
+## Show your support
+
+Give a ⭐️ if this project helped you!
+
+***
+_This README was generated with ❤️ by [readme-md-generator](https://github.com/kefranabg/readme-md-generator)_
--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -17,6 +17,7 @@ var connection = mysql.createConnection({
 
   connection.connect(function(err){
       if (err) throw err
+      
       afterConnection();
   })
 
@@ -26,21 +27,71 @@ var connection = mysql.createConnection({
           if (err) throw err
           
           var choiceArray=[]
+          var resultArray=[]
+          var priceArray=[]
          
           for (let i = 0; i < resp.length; i++) {
-              choiceArray.push(`ID: ${resp[i].id} Product Name: ${resp[i].product_name} Price: ${resp[i].price}`)
-            
+              choiceArray.push(`ID: ${resp[i].id} || Product Name: ${resp[i].product_name} || Price: ${resp[i].price} || Quantity on Hand: ${resp[i].stock_quantity}`)
+              resultArray.push(resp[i].product_name)
+              priceArray.push(resp[i].price)
           }
+          console.log('Here is the following items:')
+          console.log('\n**************************')
           console.log(choiceArray)
+          console.log('\n**************************')
         inquirer
         .prompt([
            
             {
                 name:'choices',
-                message:'What would you like to buy?!',
-                type:'list',
-                choices:choiceArray
+                message:'What would you like to buy? Input ID # of the item.',
+                
+                
+            },
+            {
+                name:'itemqty',
+                message:'How many would you like to buy?'
             }
-        ])
+        ]).then(function(resp,err){
+            if (err) throw err
+            if(resp.choices>10){
+                console.log(`Sorry there is no item that exists for ID ${resp.choices}`)
+                afterConnection();
+            }else
+            console.log(resp.choices)
+            var stock=`stock_quantity=stock_quantity-`+resp.itemqty
+            var total=priceArray[resp.choices-1]*resp.itemqty
+            connection.query(
+                `UPDATE products SET ${stock} WHERE ?`,
+                [
+                    {
+                        id:resp.choices
+                    }
+                ],function(err,resp){
+                    if(err) throw err;
+                 
+                }
+
+                )
+                console.log(`Congratulations you have purchased ${resp.itemqty} of ${resultArray[resp.choices -1]}`) 
+                console.log(`Your total for this order is ${total}`)
+                console.log('\n')
+                inquirer
+                .prompt(
+                    [
+                        {
+                            name:'again',
+                            message:'Would you like to make another purchase?',
+                            type:'confirm'
+                        }
+                    ]
+                ).then(function(resp,err){
+                    if (err) throw err
+                    if(resp.again){
+                        afterConnection();
+                    }else
+                    connection.end();
+                })
+        })
       })
   }
