d4ccc2ffa8d8e459bc240316e64c4defa390ae7a
Jester <ABooth9422@gmail.com>
2019-07-07 20:37:23 -0400

    basic completed

--- bamazon.sql
+++ bamazon.sql
@@ -14,13 +14,13 @@ CREATE TABLE products(
 );
 
 INSERT INTO products(product_name,department_name,price,stock_quantity)
-VALUES('sword of a thousand truths','mythic',999.99,1),
-('leeroys path of fury','rare',49.99,20),
-('recursive bow','common',19.99,50),
-('unstoppable force','rare',60.00,10),
-('zulian tiger','mythic',400.00,2),
-('Path to Coding','legendary',200.00,15),
-('MYSQL for dummies','common',100.00,100),
-('tome of knowledge','legendary',125.00,5),
-('great feast','rare',75.00,30),
-('needlessly large rod','legendary',250.00,35);
+VALUES('Sword of a Thousand Truths','Mythic',999.99,1),
+('Leeroys Path of Fury','Rare',49.99,20),
+('Recursive Bow','Common',19.99,50),
+('Unstoppable Force','Rare',60.00,10),
+('Zulian Tiger','Mythic',400.00,2),
+('Path to Coding','Legendary',200.00,15),
+('MYSQL for dummies','Common',100.00,100),
+('Tome of Knowledge','Legendary',125.00,5),
+('Great Feast','Rare',75.00,30),
+('Needlessly Large Rod','Legendary',250.00,35);
--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -1,97 +1,139 @@
-var mysql=require('mysql')
-var inquirer=require('inquirer')
+var mysql = require('mysql')
+var inquirer = require('inquirer')
+var chalk = require('chalk')
+
+var startColor = chalk.whiteBright.bgGreen
+var invColor=chalk.black.bgRed
+const log = console.log
+
+
 
 var connection = mysql.createConnection({
     host: "localhost",
-  
+
     // Your port; if not 3306
     port: 3306,
-  
+
     // Your username
     user: "root",
-  
+
     // Your password
     password: "Wow0148407!",
     database: "bamazon"
-  });
+});
 
-  connection.connect(function(err){
-      if (err) throw err
-      
-      afterConnection();
-  })
+connection.connect(function (err) {
+    if (err) throw err
 
-  function afterConnection(){
-      var query='SELECT id,product_name,price,stock_quantity FROM products'
-      connection.query(query,function(err,resp){
-          if (err) throw err
-          
-          var choiceArray=[]
-          var resultArray=[]
-          var priceArray=[]
-         
-          for (let i = 0; i < resp.length; i++) {
-              choiceArray.push(`ID: ${resp[i].id} || Product Name: ${resp[i].product_name} || Price: ${resp[i].price} || Quantity on Hand: ${resp[i].stock_quantity}`)
-              resultArray.push(resp[i].product_name)
-              priceArray.push(resp[i].price)
-          }
-          console.log('Here is the following items:')
-          console.log('\n**************************')
-          console.log(choiceArray)
-          console.log('\n**************************')
+    afterConnection();
+})
+
+function afterConnection() {
+    var query = 'SELECT id,product_name,price,stock_quantity FROM products'
+    
+    connection.query(query, function (err, resp) {
+        if (err) throw err
+
+        var choiceArray = []
+        var resultArray = []
+        var priceArray = []
+        var currentStock=[]
+
+        for (let i = 0; i < resp.length; i++) {
+            choiceArray.push(`ID: ${resp[i].id} || Product Name: ${resp[i].product_name} || Price: ${resp[i].price} || Quantity on Hand: ${resp[i].stock_quantity}`)
+            resultArray.push(resp[i].product_name)
+            priceArray.push(resp[i].price)
+            currentStock.push(resp[i].stock_quantity)
+        }
+
+        console.log('\n**************************')
+        console.log('Here is the following items that you can purchase:')
+        console.log(choiceArray)
+        console.log('\n**************************')
         inquirer
-        .prompt([
-           
-            {
-                name:'choices',
-                message:'What would you like to buy? Input ID # of the item.',
-                
-                
-            },
-            {
-                name:'itemqty',
-                message:'How many would you like to buy?'
-            }
-        ]).then(function(resp,err){
-            if (err) throw err
-            if(resp.choices>10){
-                console.log(`Sorry there is no item that exists for ID ${resp.choices}`)
-                afterConnection();
-            }else
-            console.log(resp.choices)
-            var stock=`stock_quantity=stock_quantity-`+resp.itemqty
-            var total=priceArray[resp.choices-1]*resp.itemqty
-            connection.query(
-                `UPDATE products SET ${stock} WHERE ?`,
-                [
-                    {
-                        id:resp.choices
-                    }
-                ],function(err,resp){
-                    if(err) throw err;
-                 
+            .prompt([{
+                    name: 'choices',
+                    message: 'What would you like to buy? Input ID # of the item.',
+                },
+                {
+                    name: 'itemqty',
+                    message: 'How many would you like to buy?'
                 }
+            ]).then(function (resp, err) {
+                if (err) throw err
+                if (resp.choices > 10) {
+                    console.log(`Sorry there is no item that exists for ID ${resp.choices}`)
+                    afterConnection();
+                } else if (resp.choices <= 10) {
+                   // inventoryAvailable(resp.choices-1,resp.itemqty)
+                    var inventory= currentStock[resp.choices-1]
+                    var amount= resp.itemqty
+                    if(amount>inventory){
+                       log(invColor.bold("Not enough inventory available to complete this order, Try Again!"))
+                    }else{
 
-                )
-                console.log(`Congratulations you have purchased ${resp.itemqty} of ${resultArray[resp.choices -1]}`) 
-                console.log(`Your total for this order is ${total}`)
-                console.log('\n')
-                inquirer
-                .prompt(
-                    [
-                        {
-                            name:'again',
-                            message:'Would you like to make another purchase?',
-                            type:'confirm'
+                    
+                    var stock = `stock_quantity=stock_quantity-` + resp.itemqty
+                    var total = priceArray[resp.choices - 1] * resp.itemqty
+                    connection.query(
+                        `UPDATE products SET ${stock} WHERE ?`,
+                        [{
+                            id: resp.choices
+                        }],
+                        function (err, resp) {
+                            if (err) throw err;
+
                         }
-                    ]
-                ).then(function(resp,err){
-                    if (err) throw err
-                    if(resp.again){
-                        afterConnection();
-                    }else
-                    connection.end();
-                })
-        })
-      })
-  }
+
+                    )
+                    log(startColor.bold(`Congratulations you have purchased ${resp.itemqty} of ${resultArray[resp.choices -1]}`))
+                    log(startColor.bold(`The total for your order is: $ ${total}`))
+                    log('\n')
+                    }
+                    inquirer
+                        .prompt(
+                            [{
+                                name: 'again',
+                                message: 'Would you like to make another purchase?',
+                                type: 'confirm'
+                            }]
+                        ).then(function (resp, err) {
+                            if (err) throw err
+                            if (resp.again) {
+                                afterConnection();
+                            } else
+                                connection.end();
+
+                        })
+                }
+            })
+            
+    })
+//    function inventoryAvailable(choices,amount){
+        
+//         var query=`SELECT stock_quantity FROM products WHERE id=${choices}`
+//         connection.query(query,function(err,response){
+//             if(err) throw err;
+           
+//             var inventory= response[0].stock_quantity
+//             if(amount>inventory){
+//                  return false
+                    
+//             }
+           
+//         })
+       
+//     }
+}
+
+
+
+// List a set of menu options:
+// View Products for Sale
+// View Low Inventory
+// Add to Inventory
+// Add New Product
+// If a manager selects View Products for Sale, the app should list every available item: the item IDs, names, prices, and quantities.
+// If a manager selects View Low Inventory, then it should list all items with an inventory count lower than five.
+// If a manager selects Add to Inventory, your app should display a prompt that will let the manager "add more" of any item currently in the store.
+// If a manager selects Add New Product, it should allow the manager to add a completely new product to the store.
